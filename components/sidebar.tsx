5c023ae54353234a0e216f0e56882d742cc99859
Allen Wang <wboqin@gmail.com>
2023-12-03 19:05:35 +0800

    feat:Dashboard page added

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,6 +5,7 @@ import { Montserrat } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -56,6 +57,8 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname()
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -73,7 +76,8 @@ const Sidebar = () => {
                             key={route.href}
                             href={route.href}
                             className={cn(
-                                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
                             )}
                         >
                             <div className="flex items-center flex-1">
